2ac662fbfe027cc15f08583f029becd08c87913b
hash-walker <hamzafaraz1821@gmail.com>
2026-06-18 16:56:14 +0500

    refactor: implement worker pool graceful shutdown and remove unused observability files

--- metrics.go
+++
@@ -1,9 +0,0 @@
-// package main
-
-//	type Metrics struct {
-//		jobsProcessed *prometheus.CounterVec   // labels: job_type, status
-//		jobDuration   *prometheus.HistogramVec // labels: job_type
-//		activeWorkers prometheus.Gauge
-//		queueDepth    *prometheus.GaugeVec // labels: status
-//	}
-package main
